242a63557fcdc6aba0c56259a2b0d4fdc1e10266
shaziamo <60014450+shaziamo@users.noreply.github.com>
2020-02-14 17:20:12 -0500

    Update index.js

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -6,13 +6,13 @@ function Contact() {
     return(<div>
         <Container>
         <Row><p className="reg-txt colored-txt contact-title">We would love to hear from you...</p></Row>
-            <Row><form className="contact-form">
-                <Col-8><input type="text" placeholder="Your Name" id="contact-name" className="form-control reg-txt mb-2 ml-sm-2 contact-form"/>
-                <input type="text" placeholder="Your Email" id="contact-email" className="form-control reg-txt mb-2 ml-sm-2 contact-form" />
-                <input type="text" placeholder="Write your comment here..." id="contact-comment" className="form-control reg-txt mb-2 ml-sm-2 contact-form" /></col-8>
-                <Col-4><button type="submit" className="btn btn-primary curs-txt mb-1 sign-up-form float-right" id="contact-btn">Send</button></col-4>
+          <form className="contact-form">
+            <Row><input type="text" placeholder="Your Name" id="contact-name" className="form-control reg-txt mb-2 ml-sm-2 contact-form"/></row>
+               <Row> <input type="text" placeholder="Your Email" id="contact-email" className="form-control reg-txt mb-2 ml-sm-2 contact-form" /></row>
+               <Row><Col><input type="text" placeholder="Write your comment here..." id="contact-comment" className="form-control reg-txt mb-2 ml-sm-2 contact-form" /></col>
+                <col><button type="submit" className="btn btn-primary curs-txt mb-1 sign-up-form float-right" id="contact-btn">Send</button></col></row>
             </form>
-        </Row>
+        
         </Container>
         
 
