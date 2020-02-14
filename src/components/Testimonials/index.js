346d87a11da9d1ce79122365d52510854ab2dfc6
shaziamo <60014450+shaziamo@users.noreply.github.com>
2020-02-14 17:44:13 -0500

    Update index.js

--- src/components/Testimonials/index.js
+++ src/components/Testimonials/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Container, Row, Col} from "../Grid";
 import API from "../../utils/API"
 import "./style.css";
 
@@ -30,7 +31,7 @@ function Testimonials() {
                         </div>
                     )
                 })
-            }</container>
+            }</Container>
         </div>
 
     )
