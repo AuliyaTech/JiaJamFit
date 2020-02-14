4902be01b28317c6c71bf82a2b2ff1b27d527427
shaziamo <60014450+shaziamo@users.noreply.github.com>
2020-02-14 17:16:40 -0500

    Update index.js

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -7,10 +7,10 @@ function Contact() {
         <Container>
         <Row><p className="reg-txt colored-txt contact-title">We would love to hear from you...</p></Row>
             <Row><form className="contact-form">
-                <input type="text" placeholder="Your Name" id="contact-name" className="form-control reg-txt mb-2 ml-sm-2 contact-form"/>
+                <Col-8><input type="text" placeholder="Your Name" id="contact-name" className="form-control reg-txt mb-2 ml-sm-2 contact-form"/>
                 <input type="text" placeholder="Your Email" id="contact-email" className="form-control reg-txt mb-2 ml-sm-2 contact-form" />
-                <input type="text" placeholder="Write your comment here..." id="contact-comment" className="form-control reg-txt mb-2 ml-sm-2 contact-form" />
-                <button type="submit" className="btn btn-primary curs-txt mb-1 sign-up-form float-right" id="contact-btn">Send</button>
+                <input type="text" placeholder="Write your comment here..." id="contact-comment" className="form-control reg-txt mb-2 ml-sm-2 contact-form" /></col-8>
+                <Col-4><button type="submit" className="btn btn-primary curs-txt mb-1 sign-up-form float-right" id="contact-btn">Send</button></col-4>
             </form>
         </Row>
         </Container>
