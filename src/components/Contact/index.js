4972662dd93364f4d5e5dab212e415e9167758d6
shaziamo <60014450+shaziamo@users.noreply.github.com>
2020-02-14 12:30:07 -0500

    Update index.js

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -5,15 +5,16 @@ import "./style.css";
 function Contact() {
     return(
         <Container>
+        <Row>
             <div className="contact heading">
-            <p className="reg-txt colored-txt test-title">We would love to hear from you...</p>
-            <form className="contact-form">
+            <p className="reg-txt colored-txt test-title">We would love to hear from you...</p></Row>
+            <Row><form className="contact-form">
                 <input type="text" placeholder="Your Name" id="contact-name" className="form-control reg-txt mb-2 ml-sm-2 contact-form"/>
                 <input type="text" placeholder="Your Email" id="contact-email" className="form-control reg-txt mb-2 ml-sm-2 contact-form" />
                 <input type="text" placeholder="Write your comment here..." id="contact-comment" className="form-control reg-txt mb-2 ml-sm-2 contact-form" />
                 <button type="submit" className="btn btn-primary curs-txt mb-1 sign-up-form float-right" id="contact-btn">Send</button>
             </form>
-        </div>
+        </div></Row>
         </Container>
         
 
