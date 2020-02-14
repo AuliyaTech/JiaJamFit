e6fd26252f690c71c27d7e81f21ce0ffcf84ea8e
shaziamo <60014450+shaziamo@users.noreply.github.com>
2020-02-14 12:43:47 -0500

    Update index.js

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -6,7 +6,6 @@ function Contact() {
     return(
         <Container>
         <Row>
-            <div className="contact heading">
             <p className="reg-txt colored-txt test-title">We would love to hear from you...</p></Row>
             <Row><form className="contact-form">
                 <input type="text" placeholder="Your Name" id="contact-name" className="form-control reg-txt mb-2 ml-sm-2 contact-form"/>
@@ -14,7 +13,7 @@ function Contact() {
                 <input type="text" placeholder="Write your comment here..." id="contact-comment" className="form-control reg-txt mb-2 ml-sm-2 contact-form" />
                 <button type="submit" className="btn btn-primary curs-txt mb-1 sign-up-form float-right" id="contact-btn">Send</button>
             </form>
-        </div></Row>
+        </Row>
         </Container>
         
 
