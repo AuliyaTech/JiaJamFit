23dbce63afbe1c246c6dd9d86de5f16a71b0c788
AuliyaTech <60294866+AuliyaTech@users.noreply.github.com>
2020-02-15 18:40:08 -0500

    Update index.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,8 +4,8 @@ import "./style.css";
 function Footer(){
     return (
         <div className="footer">
-            <div className="footer-content"><p className="reg-txt">Powered by AuliyaTech LTD</p>
-            <a href="/waiver"><p className="reg-txt">Liability Waiver</p></a></div>
+            <div className="footer-content"><p className="reg-txt">&copy; Jia JAM FIT</p>
+           </div>
             
         </div>
     )
