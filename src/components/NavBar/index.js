5179ec89c0b17d81cf8180bada6564a2392f5310
m.az94@hotmail.com <maria@marias-MacBook-Air.local>
2020-02-15 18:57:12 -0500

    new image

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -8,10 +8,13 @@ function NavBar() {
     return(
         <div className="navigation">
             <nav class="navbar navbar-expand-lg navbar-toggleable-md navbar-light bg-light">
-            <Container>
-                <Row>
-                    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+                <Link to="/"><img className="logo" src={img} /></Link>
 
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div id="navbarNavDropdown" class="navbar-collapse collapse">
+            
 
                     <ul class="navbar-nav mx-auto">
 
@@ -21,9 +24,6 @@ function NavBar() {
                         <li class="nav-item mx-auto">
                             <Link class="nav-link reg-txt" to="/testimonials">TESTIMONIALS</Link>
                         </li>
-                        <li class="nav-item mx-auto logo">
-                           <Link to="/"><img className="logo" src={img} /></Link>
-                        </li>
                         <li class="nav-item mx-auto">
                             <Link class="nav-link reg-txt" to="/location">LOCATIONS</Link>
                         </li>
@@ -32,8 +32,7 @@ function NavBar() {
                         </li>
                     </ul>
                 </div>
-                </Row>
-            </Container>
+          
             </nav>
             
         </div>
@@ -42,3 +41,49 @@ function NavBar() {
 }
 
 export default NavBar;
+
+// class Nav extends Component {
+//     constructor(props) {
+//       super(props);
+//       this.toggleNavbar = this.toggleNavbar.bind(this);
+//       this.state = {
+//         collapsed: true,
+//       };
+//     }
+//     toggleNavbar() {
+//       this.setState({
+//         collapsed: !this.state.collapsed,
+//       });
+//     }
+//     render() {
+//       const collapsed = this.state.collapsed;
+//       const classOne = collapsed ? 'collapse navbar-collapse' : 'collapse navbar-collapse show';
+//       const classTwo = collapsed ? 'navbar-toggler navbar-toggler-right collapsed' : 'navbar-toggler navbar-toggler-right';
+//       return (
+//       <nav className="navbar navbar-expand-lg navbar-dark bg-dark transparent-nav">
+//       <div className="container">
+//         <a className="navbar-brand" href="#">Rate My Neighborhood</a>
+//         <button  onClick={this.toggleNavbar} className={`${classTwo}`} type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+//             <span className="navbar-toggler-icon" />
+//           </button>
+//           <div className={`${classOne}`} id="navbarResponsive">
+//             <ul className="navbar-nav ml-auto">
+//               <li className="nav-item active">
+//                 <Link className="nav-link" to="/">Home</Link>
+//               </li>
+//               <li className="nav-item">
+//                 <Link className="nav-link" to="/about">About</Link>
+//               </li>
+//             </ul>
+//           </div>
+//       </div>
+//     </nav>
+//       );
+//     }
+//   }
+  
+//   export default Nav;
+  
+
+
+
