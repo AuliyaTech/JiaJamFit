2ee4d7c8becd8643026f3e2f1cf1528251a8e46d
m.az94@hotmail.com <maria@marias-MacBook-Air.local>
2020-02-15 20:01:55 -0500

    nav

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -9,29 +9,43 @@ function NavBar() {
         <div className="navigation">
             <nav class="navbar navbar-expand-lg navbar-toggleable-md navbar-light bg-light">
                 <Link to="/"><img className="logo" src={img} /></Link>
-
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div id="navbarNavDropdown" class="navbar-collapse collapse">
-            
-
-                    <ul class="navbar-nav mx-auto">
-
-                        <li class="nav-item mx-auto">
+                <div id="navbarNavDropdown" class="navbar-collapse collapse"></div>
+                <ul class="navbar-nav mx-auto">
+                        <li class="nav-item dropdown-item mx-auto">
                                 <a href="https://auliyatech.github.io/JiaMailChimp/" class="nav-link reg-txt">REGISTER</a>
                         </li>
                         <li class="nav-item mx-auto">
-                            <Link class="nav-link reg-txt" to="/testimonials">TESTIMONIALS</Link>
+                            <Link class="nav-link dropdown-item reg-txt" to="/testimonials">TESTIMONIALS</Link>
                         </li>
-                        <li class="nav-item mx-auto">
+                        <li class="nav-item dropdown-item mx-auto">
                             <Link class="nav-link reg-txt" to="/location">LOCATIONS</Link>
                         </li>
-                        <li class="nav-item mx-auto">
+                        <li class="nav-item dropdown-item mx-auto">
                             <Link class="nav-link reg-txt" to="/contact">CONTACT</Link>
                         </li>
-                    </ul>
-                </div>
+                </ul>
+
+                
+            
+
+             {/* <ul class="navbar-nav dropdown-menu mx-auto">
+                        <li class="nav-item dropdown-item mx-auto">
+                                <a href="https://auliyatech.github.io/JiaMailChimp/" class="nav-link reg-txt">REGISTER</a>
+                        </li>
+                        <li class="nav-item mx-auto">
+                            <Link class="nav-link dropdown-item reg-txt" to="/testimonials">TESTIMONIALS</Link>
+                        </li>
+                        <li class="nav-item dropdown-item mx-auto">
+                            <Link class="nav-link reg-txt" to="/location">LOCATIONS</Link>
+                        </li>
+                        <li class="nav-item dropdown-item mx-auto">
+                            <Link class="nav-link reg-txt" to="/contact">CONTACT</Link>
+                        </li>
+                </ul> */}
+            
           
             </nav>
             
